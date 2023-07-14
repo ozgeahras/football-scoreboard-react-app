c79dc79cabf11b404c655a587f1107b6a5d3fa80
ozgeahras <ozgeahras@gmail.com>
2023-07-14 09:08:16 +0300

    added scoreboardgrid component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import classes from "./app.module.scss";
+import ScoreboardsGrid from "./components/ScoreboardsGrid";
+import Footer from "./components/Footer/index.jsx";
+import Header from "./components/Header";
 
 function App() {
-  return <div className={classes.app}></div>;
+  return (
+    <div className={classes.app}>
+      <Header />
+      <main>
+        <ScoreboardsGrid />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
